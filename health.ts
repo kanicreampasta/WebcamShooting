e6f8dc4b54f46140842d01d0271a9ed4348f1f23
das08 <41512077+das08@users.noreply.github.com>
2022-03-24 20:38:11 +0900

    add: added `heal` function

--- health.ts
+++ health.ts
@@ -1,38 +1,54 @@
 type HealthType = "armour" | "flesh";
 
 const MAX_ARMOUR_HEALTH = [
-  {level: 0, value: 0},
+  {level: 0, value: 10},
   {level: 1, value: 20},
   {level: 2, value: 50},
-]
+];
+const MAX_FLESH_HEALTH = 50;
 
 export class PlayerHealth {
   armourLevel: number;
   remainingHealth : {
-    healthType: HealthType,
-    value: number
+    armour: number,
+    flesh: number
   };
   isHealing : {
     status: boolean,
-    healTarget : HealthType
+    healTarget? : HealthType
   };
 
-  constructor(remainingHealth: number) {
-    this.armourLevel = 0;
+  constructor(armourLevel: number) {
+    this.armourLevel = armourLevel;
     this.remainingHealth = {
-      healthType : "flesh",
-      value: remainingHealth
+      armour: this.getMaxArmourValue(armourLevel),
+      flesh: MAX_FLESH_HEALTH
     }
   }
 
-  healed(healthType: HealthType): boolean {
+  heal(healthType: HealthType): boolean {
     let isHealed = false;
     switch (healthType) {
       case "armour":
+        const maxArmourValue = this.getMaxArmourValue(this.armourLevel);
+        if (this.remainingHealth.armour < maxArmourValue) {
+          this.remainingHealth.armour = maxArmourValue;
+          isHealed = true;
+        }
         break;
       case "flesh":
+        if (this.remainingHealth.flesh < MAX_FLESH_HEALTH) {
+          this.remainingHealth.flesh = MAX_FLESH_HEALTH;
+          isHealed = true;
+        }
         break;
     }
     return isHealed;
+  }
+
+  private getMaxArmourValue(armourLevel): number {
+    const armour = MAX_ARMOUR_HEALTH.find(e => e.level === armourLevel);
+    if (armour == undefined) return 0
+    return armour.value;
   }
 }
