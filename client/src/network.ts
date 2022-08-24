48cf78b27c6013689064162a4bf7a193c9fcf142
tinaxd <31384595+tinaxd@users.noreply.github.com>
2022-08-24 15:31:54 +0900

    strict

--- client/src/network.ts
+++ client/src/network.ts
@@ -36,8 +36,8 @@ const GAME_SERVER = "ws://localhost:5000";
 const VIDEO_SERVER = "http://localhost:5001/janus";
 
 export class NetworkClient {
-    private socket: WebSocket;
-    private loopKey: NodeJS.Timer;
+    private socket?: WebSocket;
+    private loopKey?: NodeJS.Timer;
     private pid: string | null;
     private fired: boolean = false;
     private damageQueue: Map<string, DamageInfo> = new Map();
@@ -55,7 +55,7 @@ export class NetworkClient {
     }
 
     constructor() {
-        this.socket = null;
+        this.socket = undefined;
         this.pid = null;
     }
 
@@ -63,16 +63,18 @@ export class NetworkClient {
         this.socket = new WebSocket(GAME_SERVER);
         this.socket.addEventListener("message", (ev) => this.onmessage(ev));
         return new Promise((resolve) => {
-            this.socket.onopen = (ev) => resolve();
+            this.socket!.onopen = (ev) => resolve();
         });
     }
 
     async initVideoServer(): Promise<void> {
-        video.setOnVideoStream((stream, pid) =>
-            this.onvideostream(stream, pid)
-        );
+        video.setOnVideoStream((stream, pid) => {
+            if (this.onvideostream) {
+                this.onvideostream(stream, pid);
+            }
+        });
         await video.initJanus();
-        if (this.pid !== undefined) {
+        if (this.pid !== null) {
             video.initiateSession(VIDEO_SERVER, this.pid);
         } else {
             this.onmypid = (pid) => {
@@ -88,16 +90,16 @@ export class NetworkClient {
         const req = types.Request.create({
             joinRequest: requestJoin,
         });
-        this.socket.send(types.Request.encode(req).finish());
+        this.socket!.send(types.Request.encode(req).finish());
         this.loopKey = setInterval(() => this.loop(getPlayer), 1000 / 30);
     }
 
     stop() {
-        clearInterval(this.loopKey);
+        clearInterval(this.loopKey!);
     }
 
     public get myPid(): string {
-        return this.pid;
+        return this.pid!;
     }
 
     private loop(getPlayer: PlayerGetter) {
@@ -130,7 +132,7 @@ export class NetworkClient {
             u.damages = [];
         }
         for (const damage of this.damageQueue.values()) {
-            u.damages.push(
+            u.damages!.push(
                 types.Damage.create({
                     pid: damage.pid,
                     damage: damage.damage,
@@ -144,57 +146,15 @@ export class NetworkClient {
             clientUpdate: update,
         });
 
-        this.socket.send(types.Request.encode(req).finish());
+        this.socket!.send(types.Request.encode(req).finish());
     }
 
     private onmessage(ev: MessageEvent<any>) {
-        // console.log(ev);
-        const data = JSON.parse(ev.data);
-        // console.log(data);
-        if (typeof data !== "object") return;
-        const type = data["type"];
-        if (typeof type !== "string") return;
-
-        // console.log(ev.data.toString());
-
-        switch (type) {
-            case "pid": {
-                const pid = data["pid"];
-                if (typeof pid !== "string") {
-                    console.warn("invalid pid data");
-                    return;
-                }
-                this.pid = pid;
-                console.log(`pid set to ${pid}`);
-                if (this.onmypid !== undefined) {
-                    this.onmypid(pid);
-                }
-                break;
-            }
-            case "update": {
-                const players = data["players"];
-                if (players === undefined) {
-                    console.warn("invalid players data");
-                    return;
-                }
-                for (const player of players) {
-                    this.processPlayer(player);
-                }
-                break;
-            }
-            case "leave": {
-                if (this.onplayerdelete === undefined) {
-                    console.warn("onplayerdelete not set");
-                    return;
-                }
-                const pid = data["pid"];
-                if (pid === undefined || typeof pid !== "string") {
-                    console.warn("invalid message");
-                    return;
-                }
-                this.onplayerdelete(pid);
-                break;
-            }
+        const bin = ev.data;
+        const res = types.Response.decode(bin);
+        if (res.pidResponse) {
+            const pid = res.pidResponse.pid;
+        } else if (res.updateResponse) {
         }
     }
 
@@ -211,16 +171,16 @@ export class NetworkClient {
             console.warn("enemy has undefined PID");
             return;
         }
-        if (!this.damageQueue.has(playerToHurt.pid)) {
-            this.damageQueue.set(playerToHurt.pid, {
-                pid: playerToHurt.pid,
+        if (!this.damageQueue.has(playerToHurt.pid!)) {
+            this.damageQueue.set(playerToHurt.pid!, {
+                pid: playerToHurt.pid!,
                 damage,
                 afterHP,
             });
         } else {
-            const info = this.damageQueue.get(playerToHurt.pid);
-            info.damage += damage;
-            info.afterHP = afterHP;
+            const info = this.damageQueue.get(playerToHurt.pid!);
+            info!.damage += damage;
+            info!.afterHP = afterHP;
         }
     }
 
